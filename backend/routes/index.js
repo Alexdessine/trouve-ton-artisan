30079103f110bb2a479d6b81642de5eb8a7207cd
Alexdessine <alexandre@bourlier.email>
2026-01-19 09:37:15 +0100

    Initialisation de la route /api

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.status(200).json({ message: 'Backend running' });
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/api', function(req, res, next) {
+  res.json({ message: 'Welcome to the API' });
 });
 
 module.exports = router;
