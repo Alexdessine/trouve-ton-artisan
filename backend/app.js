90d5dfb2d6bc88e8f0bcbc965d65f894e6f60b7a
Alexdessine <alexandre@bourlier.email>
2026-01-20 10:42:33 +0100

    ajout des constantes globalLimiter dans le fichier .env

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,18 @@
 require('dotenv').config({ path: './src/config/database' });
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const { globalLimiter } = require('./src/middlewares/rateLimit.middleware');
 
 var indexRouter = require('./src/routes/index');
 var usersRouter = require('./src/routes/users');
 var apiRouter = require('./src/routes/api.routes');
 
 var app = express();
-require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 console.log("MAIL_FROM =", process.env.MAIL_FROM);
 console.log("MAIL_TO =", process.env.MAIL_TO);
 console.log("SMTP_HOST =", process.env.SMTP_HOST);
@@ -21,6 +22,8 @@ console.log("SMTP_SECURE =", process.env.SMTP_SECURE);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.set("trust proxy", 1); // nécessaire si derrière un reverse proxy (ex: Heroku, Vercel, etc.)
+app.use("/api", globalLimiter); // Applique le limiteur global à toutes les routes /api
 
 app.use(logger('dev'));
 app.use(express.json());
