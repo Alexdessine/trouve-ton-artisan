23984833a682944b15512525dd47babf16fbe472
Alexdessine <alexandre@bourlier.email>
2026-01-26 09:40:24 +0100

    Ajout de la page contact de l'artisan

--- frontend/src/components/Artisans/ArtisanCard.jsx
+++ frontend/src/components/Artisans/ArtisanCard.jsx
@@ -1,12 +1,15 @@
 import RatingStars from "./RatingStars";
+import { Link } from "react-router-dom";
 
 export default function ArtisanCard({ artisan }) {
+    const id = artisan.id_artisan;
     const name = artisan.nom;
     const city = artisan.ville;
     const specialityLabel = artisan.Specialite?.label ?? "—";
     // const favori = artisan.is_favori ? "Au top" : "";
 
     return (
+        <Link to ={`/artisans/${id}`} className="text-decoration-none" aria-label={`Voir la fiche de ${artisan?.nom ?? "cet artisan"}`}>
         <article className="tta-card p-3">
             <h3 className="h6 mb-2 tta-card__name">{name}</h3>
 
@@ -19,5 +22,6 @@ export default function ArtisanCard({ artisan }) {
                 <div>{city}</div>
             </div>
         </article>
+        </Link>
     );
 }
